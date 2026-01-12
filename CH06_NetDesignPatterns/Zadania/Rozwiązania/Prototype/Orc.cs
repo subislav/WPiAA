63c90003534fda0035dae03ff85d92b687ce4911
subislav <subczyn@gmail.com>
2026-01-12 15:48:53 +0100

    Rozwiązanie zadania czwartego: Prototype.

---
+++ CH06_NetDesignPatterns/Zadania/Rozwiązania/Prototype/Orc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Orc
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Health { get; set; }
+        public void ShowStats()
+        {
+            Console.WriteLine($"Ork: {Name} ; Siła: {Strength} ; Życie: {Health}");
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/Zadania/Rozwiązania/Prototype/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc prototypeOrc = new Orc
+            {
+                Name = "Uruk-hai",
+                Strength = 10,
+                Health = 100
+            };
+            prototypeOrc.ShowStats();
+            Console.WriteLine("Klony:");
+            List<Orc> army = new List<Orc>();
+            Random random = new Random();
+            int rnd = random.Next(3, 10);
+            for (int i = 0; i < rnd; i++)
+            {
+                string jsonString = JsonConvert.SerializeObject(prototypeOrc);
+                Orc clonedOrc = JsonConvert.DeserializeObject<Orc>(jsonString);
+                clonedOrc.Name = $"{prototypeOrc.Name} - Klon #{i + 1}";
+                clonedOrc.Strength = random.Next(4, 16);
+                clonedOrc.Health = random.Next(60, 150);
+                army.Add(clonedOrc);
+            }
+            foreach (var orc in army)
+            {
+                orc.ShowStats();
+            }
+            Console.ReadKey();
+        }
+    }
+}
